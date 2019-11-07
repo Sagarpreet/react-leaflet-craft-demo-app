d569b07754610d561dcaf0768554054feb07f181
sagarpreet-chadha <sagarpreet.chadha@flipkart.com>
2019-11-07 15:40:27 +0530

    onCreatEnd added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import React, { useState }  from 'react';
+import React from 'react';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
-import { Map, TileLayer, ZoomControl } from 'react-leaflet';
-import Freedraw, { ALL } from 'react-leaflet-craft';
+import { Map, TileLayer } from 'react-leaflet';
+import Freedraw from 'react-leaflet-craft';
+import { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class App extends Component {
+  
+  componentDidMount() {
+    document.addEventListener('keydown', event => {
+      // Cancel the current FreeDraw action when the escape key is pressed.
+      if (event.key === 'Escape') {
+        this.freedrawRef.current.leafletElement.cancel();
+      }
+    });
+  }
 
 
-function App() {
+  // Listen for any markers added, removed or edited, and then output the lat lng boundaries.
+  handleOnMarkers = event => {
+    console.log(
+      'LatLngs:',
+      event.latLngs,
+      'Polygons:',
+      this.freedrawRef.current.leafletElement.size()
+    );
+  };
 
-  // const [state, setState] = useState("a");
+  onCreateEnd = (data) => {
+    // const undo_event = new CustomEvent('undo', { key: 'z', metaKey: true, shiftKey: false });
+    // const redo_event = new CustomEvent('redo', { key: 'z', metaKey: true, shiftKey: true });
+    // document.dispatchEvent(undo_event);
+     console.log('onCreateEnd');
+     document.querySelector("div[title='Undo']").click();
+     document.querySelector("div[title='Redo']").click();
+  }
 
-  // const change = () => {
-  //   setState("b");
-  //   console.log("new State is ", state);
-  // }
+  // Listen for when the mode changes
+  handleModeChange = event => {
+    console.log('mode changed', event);
+  };
 
-  return (
-  <div className="mapContainer">
-    <Map
-      className="map"
-      center={[20.5937, 78.9629]}
-      zoom={5}
-      doubleClickZoom={false}
-    >
-      <TileLayer
-        url="https://api.tiles.mapbox.com/v4/mapbox.emerald/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FnYXJwcmVldDk3IiwiYSI6ImNqaXhlZjgwaDJtb3EzcW1zdDdwMzJkODcifQ.MA2YIv6VpGLLAo-QYUudTA"
-      />
-      <Freedraw
-      />
-    </Map>
-  </div>
-  );
+  freedrawRef = React.createRef();
+
+  render() {
+
+    return (
+      <div className="mapContainer"> 
+        <Map
+          className="map"
+          center={[20.5937, 78.9629]}
+          zoom={5}
+          doubleClickZoom={false}
+        >
+          <TileLayer
+            url="https://api.tiles.mapbox.com/v4/mapbox.emerald/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FnYXJwcmVldDk3IiwiYSI6ImNqaXhlZjgwaDJtb3EzcW1zdDdwMzJkODcifQ.MA2YIv6VpGLLAo-QYUudTA"
+          />
+          <Freedraw
+            onMarkers={this.handleOnMarkers}
+            onModeChange={this.handleModeChange}
+            ref={this.freedrawRef}
+            onCreateEnd={this.onCreateEnd}
+          />
+        </Map>
+      </div>
+    );
+  }
 }
-
-export default App;
